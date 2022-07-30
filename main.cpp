6b4631e02321c524085c0b98cdc690f42507f06d
くろねこ 黑猫 <63108760+AS13379@users.noreply.github.com>
2022-07-30 14:56:03 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+string keyin;
+int main()
+{
+	system("title LinuxEmulator bash.vision alpha.1.0.0");
+	cout << "welcome to LinuxEmulator bash.vision alpha.1.0.0" << endl;
+	while (true)
+	{
+		cout << "[user//LE.bash]#";
+		cin >> keyin;
+	}
+}
