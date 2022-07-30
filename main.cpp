a0491e48091b481ec4ec90c53eaadd05db03950f
くろねこ 黑猫 <63108760+AS13379@users.noreply.github.com>
2022-07-30 15:10:57 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
-string keyin;
+string keyin0,keyin1,keyin2,sysimg;
+int cnt = 0;
+char tmp[256];
+int check(keyin)
+{
+	if (keyin0 = "img" or keyin0 = "IMG"){
+		if(keyin1 = "create" or keyin1 = "CREATE"){
+			if(keyin2 = "centos" or keyin2 = "CENTOS"0){
+				strftime(tmp, sizeof(tmp), "%Y-%m-%d %H:%M:%S", localtime(&timep));
+				cout << "System CentOS Created"
+			}
+		}
+	}
+}
 int main()
 {
 	system("title LinuxEmulator bash.vision alpha.1.0.0");
@@ -8,6 +21,7 @@ int main()
 	while (true)
 	{
 		cout << "[user//LE.bash]#";
-		cin >> keyin;
+		cin >> keyin0 >> keyin1 >> keyin2;
+		check(keyin);
 	}
 }
