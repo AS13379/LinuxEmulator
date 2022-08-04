f7af10daabaca2cc9ff7f8f79454397e859d3efb
くろねこ 黑猫 <63108760+AS13379@users.noreply.github.com>
2022-08-04 15:36:12 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,42 +20,43 @@ void GetLocalTime()
             <<ptminfo->tm_min<<':'
             <<ptminfo->tm_sec<< ' ';
 }
-
-
-int check()
+int img()
 {
-    if (keyinlist[0] == "img" or keyinlist[0] == "IMG"){
-        if(keyinlist[1] == "create" or keyinlist[1] == "CREATE"){
-            if(keyinlist[2] == "centos" or keyinlist[2] == "CENTOS"){
-                GetLocalTime();
-                cout << "System CentOS Created" << endl << "id = " << cnt << endl << "user = centosuser" << endl << "passwd = centospasswd" << endl;
-                sysimg[cnt] = "centos";
-                cnt += 1;
-                return 0;
-            }
-            if(keyinlist[2] == "ubuntu" or keyinlist[2] == "UBUNTU"){
-                GetLocalTime();
-                cout << "System ubuntu Created" << endl << "id = " << cnt << endl << "user = ubuntu" << endl << "passwd = password123" << endl;
-                sysimg[cnt] = "ubuntu";
-                cnt += 1;
-                return 0;
-            }
-            if(keyinlist[2] == "debian" or keyinlist[2] == "DEBIAN"){
-                GetLocalTime();
-                cout << "System debian Created" << endl << "id = " << cnt << endl << "user = deuser" << endl << "passwd = password123d" << endl;
-                sysimg[cnt] = "debian";
-                cnt += 1;
-                return 0;
-            }
+    if(keyinlist[1] == "create" or keyinlist[1] == "CREATE"){
+        if(keyinlist[2] == "centos" or keyinlist[2] == "CENTOS"){
+            GetLocalTime();
+            cout << "System CentOS Created" << endl << "id = " << cnt << endl << "user = centosuser" << endl << "passwd = centospasswd" << endl;
+            sysimg[cnt] = "centos";
+            cnt += 1;
+            return 0;
         }
-        if(keyinlist[1] == "list" or keyinlist[1] == "LIST"){
-            for(int i = 0; i < 256; i++){
-                if(sysimg[i] != ""){cout << sysimg[i] << ",";}
-            }
-            cout << endl;
+        if(keyinlist[2] == "ubuntu" or keyinlist[2] == "UBUNTU"){
+            GetLocalTime();
+            cout << "System ubuntu Created" << endl << "id = " << cnt << endl << "user = ubuntu" << endl << "passwd = password123" << endl;
+            sysimg[cnt] = "ubuntu";
+            cnt += 1;
+            return 0;
+        }
+        if(keyinlist[2] == "debian" or keyinlist[2] == "DEBIAN"){
+            GetLocalTime();
+            cout << "System debian Created" << endl << "id = " << cnt << endl << "user = deuser" << endl << "passwd = password123d" << endl;
+            sysimg[cnt] = "debian";
+            cnt += 1;
             return 0;
         }
     }
+    if(keyinlist[1] == "list" or keyinlist[1] == "LIST"){
+        for(int i = 0; i < 256; i++){
+            if(sysimg[i] != ""){cout << sysimg[i] << ",";}
+        }
+        cout << endl;
+        return 0;
+    }
+}
+
+int check()
+{
+    if (keyinlist[0] == "img" or keyinlist[0] == "IMG"){img();}
     return 0;
 }
 int main()
@@ -80,7 +81,14 @@ int main()
 
         }
         keyinlist[ccnt] = tmp;
-
+        tmp.clear();
+        //dev_start
+//        for (int i = 0; i < 5; i++){
+//            cout << keyinlist[i] << " ";
+//
+//        }
+//        cout << endl;
+        //dev_end
         check();
     }
 }
